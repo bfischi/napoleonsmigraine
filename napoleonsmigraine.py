35378102a82702386b4496ab5d1cba9fc727f69c
bfischi <61169026+bfischi@users.noreply.github.com>
2021-07-20 14:58:07 -0400

    Changed dictionary to list for SQL commit

--- napoleonsmigraine.py
+++ napoleonsmigraine.py
@@ -42,7 +42,7 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Stations
      year INTEGER, month INTEGER, reading INTEGER)''')
 
 cur.execute('''CREATE TABLE IF NOT EXISTS Barometer
-    (id INTEGER PRIMARY KEY, year INTEGER, month INTEGER, 
+    (id INTEGER PRIMARY KEY, year INTEGER, 
     jan_read REAL, feb_read REAL, mar_read REAL, 
     apr_read REAL, may_read REAL, jun_read REAL, 
     jul_read REAL, aug_read REAL, sep_read REAL, 
@@ -59,13 +59,14 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Battles
 
 
 for line in linelist:
-    print('which line are we on?: ', line)
-    r = 1
+    print('''
+Which line are we on?: ''', line)
+    r = 0
     # extract lat & long from GRID-POINT lines
     if line.find('GRID-POINT') != -1:
         barlat = line[38:42]
         barlong = line[43:47]
-        r = 1
+        r = 0
         print('Barlat, barlong:', barlat, barlong)
 
     # process line and remove unneeded mean value for year (last item in list)
@@ -74,46 +75,49 @@ for line in linelist:
         pieces = pieces.split(' ')
         pieces.pop(-1)
         pieces = filter(None, pieces)
-        #print('Pieces list:', pieces)
 
         newbarlist = list()
         for piece in pieces:
             if piece.find('/'):
                 head, sep, tail = piece.partition('/')
-                newbarlist.append(head)
+                newbarlist.append(float(head))
         print('Newbarlist:', newbarlist)
         if newbarlist == []: break
 
-        readingdict = dict(zip(('jan_read', 'feb_read', 'mar_read', 'apr_read', 'may_read',
+        readingslist = list(map(list, zip(['year', 'jan_read', 'feb_read', 'mar_read', 'apr_read', 'may_read',
                                 'jun_read', 'jul_read', 'aug_read', 'sep_read', 'oct_read',
-                                'nov_read', 'dec_read'), (0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                                                          0.0, 0.0, 0.0, 0.0, 0.0)))
-
+                                'nov_read', 'dec_read', 'year'], [0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                                          0.0, 0.0, 0.0, 0.0])))
+        print('Initial readings list: ', readingslist)
         # extract data from processed line
-        for k, v in readingdict.items():
+        for item in readingslist:
             #print('r value:', r)
-            if r < 12:
-                monthlyread = newbarlist[r]
-                readingdict[k] = float(monthlyread)
+            if r == 0:
+                year = newbarlist[0]
+                readingslist[0][1] = int(year)
                 r = r + 1
-            else: break
+            elif 1 <= r < 12:
+                monthlyread = newbarlist[r]
+                readingslist[r][1] = monthlyread
+                r = r + 1
+            else:
+                monthlyread = newbarlist[r]
+                readingslist[r][1] = monthlyread
+                break
 
-        year = newbarlist[0]
-        readingdict['year'] = int(year)
-
-        print('Year', year)
+        #print('Year', year)
         print('length of newbarlist:', len(newbarlist))
-        print('readingdict:', readingdict)
-        print('newbarlist[r]', newbarlist[r])
-        print('monthdict:', readingdict)
+        print('readingslist:', readingslist)
+        print('Last val - newbarlist[r]', newbarlist[r])
 
     # cur.execute('INSERT OR IGNORE INTO Stations (barlat, barlong) VALUES (?, ?)', (barlat, barlong))
-    # cur.execute('INSERT OR IGNORE INTO Barometer (year, jan_read, feb_read, mar_read, apr_read,
-    # may_read, jun_read, jul_read, aug_read, sep_read, oct_read, nov_read, dec_read) VALUES (?, ?, ?, ?
-    # , ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    # (year, jan_read, feb_read, mar_read, apr_read,
-    # may_read, jun_read, jul_read, aug_read, sep_read, oct_read, nov_read, dec_read))
+    # cur.execute('INSERT OR IGNORE INTO Barometer (jan_read, feb_read, mar_read, apr_read, may_read, jun_read, jul_read, '
+    #             'aug_read, sep_read, oct_read, nov_read, dec_read, year) '
+    #             'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+    #             (jan_read, feb_read, mar_read, apr_read, may_read,
+    #              jun_read, jul_read, aug_read, sep_read, oct_read,
+    #              nov_read, dec_read, year))
+    #
     # conn.commit()
-
 
 cur.close()
